03518a226fb237cd72e93e334fd1551b3e4d369a
chandragehlot <chkl2401@gmail.com>
2021-12-09 19:12:18 +0530

    quantity updated added for menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,18 +1,44 @@
 import React from "react";
+import { useState, useLayoutEffect } from "react";
+
 
 const MenuItem = (props) => {
-  console.log("props", props);
-  const { menuitems } = props;
+  const [menu, setMenu] = useState([]);
+
+  const increaseQuantity = (menuitem) => {
+    const updatedMenu = menu.map(item => {
+      if (item.id === menuitem.id) {
+        item.quantity = item.quantity +1;
+      }
+      return item;
+    })
+    setMenu(updatedMenu)
+  }
+
+  const decreaseQuantity = (menuitem) => {
+    const updatedMenu = menu.map(item => {
+      if (item.id === menuitem.id) {
+        item.quantity = (item.quantity > 1) ? item.quantity - 1 : 1;
+      }
+      return item;
+    })
+    setMenu(updatedMenu)    
+  }
+
+  useLayoutEffect(()=>{
+    const menu1 = props.menuitems.map(item=>({...item, quantity: 1}))
+    setMenu(menu1);
+  },[props]);
 
   return (
     <div className="menuitem__cont">
-      {menuitems.length &&
-        menuitems.map((menuitem) => (
+      {menu.length &&
+        menu.map((menuitem) => (
           <div key={`${menuitem.id}`} className="menuitem__card">
             <div className="menuitem__left-sec">
               <div className="menuitem__row">
-                <div className="menuitem__foodtype-icon">{menuitem.type}</div>
-                <div className="menuitem__rating">4 star</div>
+                <div className="menuitem__foodtype-icon">{menuitem.type }</div>
+                <div className="menuitem__rating"> {' '} {menuitem.rating} {'star'}</div>
               </div>
               <div className="menuitem__row">
                 <div className="font-l">{menuitem.name}</div>
@@ -24,9 +50,9 @@ const MenuItem = (props) => {
             <div className="menuitem__right-sec">
               <div className="menuitem__imgcont"></div>
               <div className="menuitem__quantitySelector">
-                <div className="menuitem__quantity">-</div>
-                <div className="menuitem__quantity">1</div>
-                <div className="menuitem__quantity">+</div>
+                <div className="menuitem__quantity" onClick={()=>decreaseQuantity(menuitem)}>-</div>
+                <div className="menuitem__quantity">{menuitem.quantity}</div>
+                <div className="menuitem__quantity" onClick={()=>increaseQuantity(menuitem)}>+</div>
               </div>
             </div>
           </div>
