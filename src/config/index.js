a4932bd62b3be427277526b1f6e82361e5e67eea
chandragehlot <chkl2401@gmail.com>
2022-01-23 10:33:33 +0530

    aws url added

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 
 
 const config = {
-    BASE_API_URL: 'http://localhost:9000/api/v1',
+    BASE_API_URL: 'http://ec2-54-90-242-254.compute-1.amazonaws.com/api/v1',
     IMAGE_CDN_BASE_URL: 'https://ik.imagekit.io/a6n7g8ldqgi/fooddelivery',
     BANNER_IMAGE_KEY: 'banner_yxocxn3KgV1.jpeg?updatedAt=1638912407414'
 }
@@ -12,3 +12,6 @@ const getImageUrl = (imageKey) => {
 }
 
 export { config, getImageUrl};
+
+// dev : http://ec2-54-90-242-254.compute-1.amazonaws.com/api/v1
+// local : http://localhost:9000/api/v1
